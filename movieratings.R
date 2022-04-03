3fdced3861d8120311e911ea48e7a5d2d714ffb9
Jhoao <jhoao.martinez@utp.edu.co>
2022-04-03 13:43:09 -0500

    Using and interpreting boxplots

--- movieratings.R
+++ movieratings.R
@@ -118,3 +118,31 @@ t + geom_histogram(binwidth=10,
 t + geom_histogram(binwidth=10, 
                    aes (x=CriticRating), 
                    fill="White", colour="Blue")
+
+
+# ---------- Statistical Transformations
+u <- ggplot(data = movies, aes(x=CriticRating, y=AudienceRating,
+                               colour=Genre))
+u + geom_point()+ geom_smooth(fill=NA)
+
+#Boxplots
+u <- ggplot(data = movies, aes(x=Genre, y=AudienceRating,
+                               colour=Genre))
+u + geom_boxplot()
+u + geom_boxplot(size=1.2)
+u + geom_boxplot(size=1.2) + geom_jitter()
+
+#Another way:
+#alpha=Transparency
+u + geom_jitter() + geom_boxplot(size=1.2, alpha=0.5) 
+
+
+#Same graphic but for critics instead of audience
+
+u <- ggplot(data = movies, aes(x=Genre, y=CriticRating,
+                               colour=Genre))
+u + geom_boxplot()
+u + geom_boxplot(size=1.2)
+u + geom_boxplot(size=1.2) + geom_jitter()
+
+u + geom_jitter() + geom_boxplot(size=1.2, alpha=0.5) 
