31fa0951c01de82ea22505934a95ae90e0ce1711
Jhoao <jhoao.martinez@utp.edu.co>
2022-04-03 12:08:07 -0500

    Adding attributes for a better data visualization

--- movieratings.R
+++ movieratings.R
@@ -9,7 +9,7 @@ movies <- read.csv("data\\Movie-Ratings.csv", stringsAsFactors = TRUE)
 head(movies)
 
 #Changing column names for easier interaction
-colnames(movies) <- c("Film", "Genre", "CriticRating", "AudicenceRating", "Budget(Millions)", "Year")
+colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating", "BudgetMillions", "Year")
 head(movies)
 
 #Checking the structure of the dataset for initial analysis
@@ -21,3 +21,26 @@ summary(movies)
 #Is better for "Year" to be categorical so is possible to group by year, color by year, etc
 movies$Year <- factor(movies$Year)
 str(movies)
+
+# ---------- Aesthetics
+#Importing ggplot2 library
+library(ggplot2)
+
+#aes() function is for setting aesthetics
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating))
+
+#Addding geometry
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating)) + 
+  geom_point()
+
+
+#Adding colour
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating, 
+                          colour=Genre)) + 
+  geom_point()
+
+#Adding size
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating, 
+                          colour=Genre, size=BudgetMillions)) + 
+  geom_point()
+
